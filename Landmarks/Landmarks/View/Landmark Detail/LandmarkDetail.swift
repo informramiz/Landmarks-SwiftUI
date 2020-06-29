bef3a6834d6e64caee5ae02a1518345ba37b5dcc
Ramiz Raja <informramiz@gmail.com>
2020-06-30 02:18:59 +0500

    feat: Write landmark list view

---
+++ Landmarks/Landmarks/View/Landmark Detail/LandmarkDetail.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Ramiz Raja on 30/06/2020.
+//  Copyright © 2020 Ramiz Raja. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(AppData.landmarkData) { landmark in 
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
